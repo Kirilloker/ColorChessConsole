705b5acb1c75799e423553fef595daacce761754
Егор <89208912+Kirilloker@users.noreply.github.com>
2022-07-02 20:28:23 +0300

    Types!
    Add TypeGame and a little more

---
+++ ColorChessConsole/ColorChessConsole/Cell.cs
@@ -0,0 +1,93 @@
+﻿namespace ColorChessConsole;
+
+public class Cell
+{
+    //CellView cellView;
+
+    Position pos;
+
+    private int numberPlayer;
+    private CellType type;
+    private FigureType figureType;
+
+    private Cell() { }
+
+    public Cell(Position pos)
+    {
+        this.pos = pos;
+        this.numberPlayer = -1;
+        this.Type = CellType.Empty;
+        this.figureType = FigureType.Empty;
+    }
+
+
+    public void Click()
+    {
+    }
+
+    public void ShowPrompt()
+    {
+    }
+
+    public void HidePrompt()
+    {
+    }
+
+    public void ChangeColor()
+    {
+    }
+
+    public bool Avaible(Dictionary<CellType, List<int>> popaxyi)
+    {
+        // Может ли фигура наступить на такой тип клетки
+        return popaxyi[type].Contains(numberPlayer);
+    }
+
+
+
+    // Свойства
+    public int NumberPlayer
+    {
+        get{ return numberPlayer; }
+        set
+        {
+            numberPlayer = value;	
+            //cellView.Update();
+        }
+    }
+
+    public CellType Type
+    {
+        get { return type; }
+        set
+        {
+            type = value;
+            //cellView.Update();
+        }
+    }
+    public FigureType FigureType
+    {
+        get { return figureType; }
+        set
+        {
+            figureType = value;
+        }
+    }
+
+    // Для Логов
+    public override string ToString()
+    {
+        string Logs = "";
+
+        Logs += "Position: " + pos.ToString() + "\n";
+
+        Logs += "Type: " + type.ToString() + "\n";
+
+        Logs += "Number Player: " + numberPlayer + "\n";
+
+        Logs += "Figure: " + figureType.ToString() + "\n";
+
+        return Logs;
+    }
+
+}
---
+++ ColorChessConsole/ColorChessConsole/DebugConsole.cs
@@ -0,0 +1,14 @@
+﻿//using UnityEngine; 
+
+namespace ColorChessConsole
+{
+    public static class DebugConsole
+    {
+        public static void Print(string str)
+        {
+            //Debug.Log(str);
+            Console.WriteLine(str);
+        }
+
+    }
+}
