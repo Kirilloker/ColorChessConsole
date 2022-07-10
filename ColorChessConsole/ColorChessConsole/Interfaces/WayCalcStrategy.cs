073f7429d94c9a8b247cff274d14d01e9c38ce16
tealvl <70812161+tealvl@users.noreply.github.com>
2022-07-10 17:03:36 +0300

    Interface

---
+++ ColorChessConsole/ColorChessConsole/Interfaces/WayCalcStrategy.cs
@@ -0,0 +1,6 @@
+﻿namespace ColorChessConsole;
+interface WayCalcStrategy
+{
+    public List<Cell> AllSteps(Map map, Figure figure);
+    public List<Cell> Way(Map map, Position startPos, Position endPos, Figure figure);
+}
